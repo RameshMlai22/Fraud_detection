10654d0062dba86d26b0799fcb227506fde38589
RameshMlai22 <ramesh.mlai22@gmail.com>
2022-04-30 12:37:43 +0530

    model2 is the final

--- ml_model.py
+++ ml_model.py
@@ -4,10 +4,10 @@ print("ml related changes")
 
 
 
-def model1(
+def model1()
     print("adding code for model1")
-)
 
-def model2(
+def model2(0
     print("model2")
-)
+
+print("model2 is working fine")
